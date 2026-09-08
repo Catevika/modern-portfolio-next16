549f1bd9b1f24397f4154ebcec50ac853f6529b2
Catevika <dominique.bello@outlook.fr>
2026-09-08 17:00:18 +0200

    Migration start

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+});
+
+export const metadata: Metadata = {
+	title: "Portfolio Example",
+	description: "Modern & Minimalist JS Mastery portfolio",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.className} antialiased`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ src/components/Approach.tsx
@@ -0,0 +1,3 @@
+export default function approach() {
+	return <div>Approach</div>;
+}
